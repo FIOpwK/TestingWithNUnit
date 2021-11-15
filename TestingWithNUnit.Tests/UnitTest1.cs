850a847fd31338ef48242e1c591b10f25a23a369
fiopwk <dajuan-mcdonald@lambdastudents.com>
2021-11-15 13:40:36 -0500

    NUnit Assertion practice

--- TestingWithNUnit.Tests/UnitTest1.cs
+++ TestingWithNUnit.Tests/UnitTest1.cs
@@ -14,8 +14,38 @@ namespace TestingWithNUnit.Tests
             [Test]
             public void AreEqual()
             {
-                Assert.AreEqual(expected: "expected", actual: "actual");
+                Assert.AreEqual(4, actual: 4);
             
+            }
+
+            [Test]
+            public void AreEqualNumbersWithinTolerance()
+            {
+                Assert.AreEqual(2,2d,.5);
+            }
+
+            [Test]
+            public void IsInputValidType()
+            {
+                Assert.Pass();
+            }
+
+            [Test]
+            public void AreEqualArrays()
+            {
+                var expected = new int[] {1, 2, 3};
+                var actual = new int[] {1, 3, 2};
+                Array.Sort(actual);
+                Assert.AreEqual(expected, actual);
+            }
+
+            [Test]
+            public void AreReferencesEqual()
+            {
+                var obj1 = new object();
+                var obj2 = obj1;
+                
+                Assert.AreSame(obj1, obj2);
             }
      }      
 }
